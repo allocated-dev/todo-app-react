247e34839ffa251d2e0eda92e42b0587444f2f88
Asif Khan <asifkhn2000@gmail.com>
2025-11-24 13:58:15 +0500

    Update about us

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
         <div className="bg-gray-300 flex items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">About Us Page</h1>
+            <h1 className="text-4xl font-bold">About Us</h1>
         </div>
     )
 }
