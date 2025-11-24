f8846390d3f15112463bef24879da564b21669f1
Asif Khan <asifkhn2000@gmail.com>
2025-11-24 13:51:19 +0500

    Create about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
         <div className="bg-gray-300 flex items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">This is the About Page</h1>
+            <h1 className="text-4xl font-bold">About Page</h1>
         </div>
     )
 }
